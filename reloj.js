0d48a32abfa0526c0db07eee0733ecd8d7b183d1
Your Name <you@example.com>
2025-04-02 21:56:18 +0200

    faltan frases y css

--- reloj.js
+++ reloj.js
@@ -1,6 +1,6 @@
-timeContainer = document.getElementById('time-container'); 
+container = document.getElementById('container'); 
 
-const setClock = () => { 
+function setClock() {
     let currentDate = new Date();
     let hours = currentDate.getHours();
     let minutes = currentDate.getMinutes();
@@ -9,16 +9,25 @@ const setClock = () => {
     let month = currentDate.getMonth() + 1;
     let year = currentDate.getFullYear();
 
+    ({ hours, minutes, seconds, day, month } = fixTimeAndDate(hours, minutes, seconds, day, month));
+
+    const time = `${hours} : ${minutes} : ${seconds} `;
+    const date = `${day} / ${month} / ${year}`;
+    container.innerHTML = `
+    <div class="time">${time}</div>
+    <div><p class="phrases">Aqui iran las frases</P></div>
+    <div class="date">${date}</div>`;
+}
+
+setInterval(setClock, 1000);
+setClock();
+
+function fixTimeAndDate(hours, minutes, seconds, day, month) {
     hours = hours < 10 ? '0' + hours : hours;
     minutes = minutes < 10 ? '0' + minutes : minutes;
     seconds = seconds < 10 ? '0' + seconds : seconds;
     day = day < 10 ? '0' + day : day;
     month = month < 10 ? '0' + month : month;
-
-    const time = `${hours} : ${minutes} : ${seconds} `;
-    const date = `${day} / ${month} / ${year}`;
-    timeContainer.innerHTML = `${time} ${date}`;
+    return { hours, minutes, seconds, day, month };
 }
 
-setInterval(setClock, 1000);
-setClock();
