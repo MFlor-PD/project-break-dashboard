e8327dc64895f1c2507fa9c3c0d4a9e0c101154a
Your Name <you@example.com>
2025-04-02 22:53:08 +0200

    falta terminar las frases

--- reloj.js
+++ reloj.js
@@ -1,7 +1,7 @@
-container = document.getElementById('container'); 
+container = document.getElementById('container');
 
 function setClock() {
-    let currentDate = new Date();
+    let currentDate = new Date()
     let hours = currentDate.getHours();
     let minutes = currentDate.getMinutes();
     let seconds = currentDate.getSeconds();
@@ -13,14 +13,19 @@ function setClock() {
 
     const time = `${hours} : ${minutes} : ${seconds} `;
     const date = `${day} / ${month} / ${year}`;
+    //const phrase =                                                            <div><p class="phrases">${phrase}</P></div>
     container.innerHTML = `
     <div class="time">${time}</div>
-    <div><p class="phrases">Aqui iran las frases</P></div>
+    
     <div class="date">${date}</div>`;
 }
 
 setInterval(setClock, 1000);
 setClock();
+
+//function getCurrentHourDecimal() {
+    //return hours + minutes / 60;
+  //}
 
 function fixTimeAndDate(hours, minutes, seconds, day, month) {
     hours = hours < 10 ? '0' + hours : hours;
@@ -30,4 +35,17 @@ function fixTimeAndDate(hours, minutes, seconds, day, month) {
     month = month < 10 ? '0' + month : month;
     return { hours, minutes, seconds, day, month };
 }
+
+const setPhrases = [
+    {start: 0, end: 7.99, phrase: 'A dormir cabrones!'},
+    {start: 8, end: 10.99, phrase: 'Y ahora seguro que no te puedes levantar de la cama, venga hombre!'},
+    {start: 11, end: 14.99, phrase: 'Que... encima estas pensando en comer??, de trabajar no hablamos eh?'},
+    {start: 15, end: 18.99, phrase: 'Ni se te ocurra pensar en la siesta!'},
+    {start: 19, end: 21.99, phrase: 'y si...salen unas cañitas??'},
+    {start: 22, end: 23.99, phrase: 'y ya fue... que trabaje Raquel, mirate una buena peli!'},
+];
+
+
+
+    
 
