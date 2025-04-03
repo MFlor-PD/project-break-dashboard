7bf50684b0f47c502da8613ef6a2ed3974eba5dc
Your Name <you@example.com>
2025-04-03 18:56:14 +0200

    terminado, falta CSS

--- password.js
+++ password.js
@@ -1,16 +1,16 @@
-    const displayPassword = document.getElementById('display-password');
+    const displayPassword = document.getElementById('display-password');               //Captura de elementos del DOM 
     const passLenght = document.getElementById('pass-lenght');
     btn = document.getElementById('btn');
 
-    //declarar las variables
-    const upperCases = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  
+    const upperCases = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';                                     //Declarar variables con datos que necesitare
     const lowerCases = 'abcdefghijklmnopqrstuvwxyz';
     const numbers = '0123456789';
     const symbols = '!@#$%^&*()-_=+';
-    const all = upperCases + lowerCases + numbers + symbols;
+    const all = upperCases + lowerCases + numbers + symbols;                               //Integrar TODOS los datos de las distintas variables en una sola. 
 
-    //ESTA FUNCION TODA, GENERA LA CONTRASENA, ahora con 12 caracteres
-const setPassword = (length = 50) => {                                             //en esteLENGHT esta la papa
+//ESTA FUNCION TODA, GENERA LA CONTRASENA, ahora con 12 caracteres
+const setPassword = (length = 12) => {                                                   //length el valor minimo, para no buguear el bucle
     
     let password = "";                                                                  //declarar la variable password vacia a llenarse con lo de abajo
     
@@ -25,41 +25,26 @@ const setPassword = (length = 50) => {                                          
      
     password = password.split('').sort(() => Math.random() - 0.5).join('');           // Toma todo lo que hay en pasword, lo divide, lo mezcla y lo vuelve a unir
     
-    displayPassword.innerHTML = `
-    <div>
-      <p>Tu nueva constraseña es: </p>
-      <div class='password-display'>${password}</div>
-      <p>No olvides guardarla en un lugar seguro para que no se te olvide</p>
-    </div>`
+    
     return password;                                                                  //Devuelve la cajita pasword con los 12 caracteres mezclados
     
 };
-setPassword();
+//ESTA F(X) OBTIENE EL LARGO MINIMO Y MAXIMO DE CONTRASENIA Y DEVUELVE MENSAJES
+const getPasswordLength = () => {
+let passwordLength = parseInt(passLenght.value);                                      //parse y value para ingresar los valores dentro del input
 
-//NECESITO CONFIGURAR PARA QUE LA CONTRASENA SEA ENTRE 12 Y 50.
-
-// * if numeroElegidoInput >= 12 && <=50 =>IMPRIMA UNA COSNTRASENA
-// * else numeroeleigoInput < 12 || > 50 => 'Numero invalido'
-
-
-
-/*function setPasswordLenght(password) {
-if (passLenght  >= 12 && passLenght  <= 50) {
-
-} else (passLenght  < 12 || passLenght  > 50) {
- displayPassword.innerHTML = 'Introduce un número válido entre 12 y 50 caracteres'}
+if (passwordLength < 12 || passwordLength > 50) {
+    displayPassword.innerHTML = 'Introduce un número válido entre 12 y 50 caracteres';
+    return;
 }
-const generatedPassword = setPassword();
-setPasswordLenght(generatedPassword);*/
+const getPassword = setPassword(passwordLength);
 
-/*btn.addEventListener('click', () => {
-     (passLenght  >= 12 && passLenght  <= 50) {
-        return displayPassword.innerHTML = `
-            <div>
-            <p>Tu nueva constraseña es: </p>
-            <div class='password-display'>${password}</div>
-            <p>No olvides guardarla en un lugar seguro para que no se te olvide</p>
-            </div>`
-        } else  {
-         displayPassword.innerHTML = 'Introduce un número válido entre 12 y 50 caracteres'}
-})*/
+displayPassword.innerHTML = `
+    <div>
+      <p>Tu nueva constraseña es: </p>
+      <div class='password-display'>${getPassword}</div>
+      <p>No olvides guardarla en un lugar seguro para que no se te olvide</p>
+    </div>`
+}
+//eventos de click
+btn.addEventListener('click', getPasswordLength);
