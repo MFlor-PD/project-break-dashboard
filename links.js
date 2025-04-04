6dbbe9692140e1c7f3280a8f9aad2df3f4674dfc
Your Name <you@example.com>
2025-04-04 21:42:08 +0200

    constrasenias terminadas falta CSS

--- links.js
+++ links.js
@@ -3,11 +3,14 @@ const escribeNombreInput = document.getElementById('escribe-nombre');
 const storagebtn = document.getElementById('storage-btn');
 const ulList = document.getElementById('ul');
 
-
-
 function storeData() {
     const url = escribeUrlInput.value;
     const nombre = escribeNombreInput.value;
+
+    if (!url || !nombre) {
+        console.error("Uno de los campos está vacío");
+        return;
+    }
     const linkObject = { url, nombre };
 
     const storeItem = localStorage.getItem('data');
@@ -19,20 +22,31 @@ function storeData() {
     return saveLinks;
 }
 
-//storagebtn.addEventListener('click', storeData);
-   
-
-
-/*function renderizar(getData) {
-    getData.forEach(({url, nombre}) => { 
+function renderizar(saveLinks) {
+    ulList.innerHTML = ''; 
+    
+  saveLinks.forEach(({url, nombre}, index) => { 
         let template = `
-        <li class='link-item'>
-         <a href='${url}' target='_blank'>${nombre}</a>
-         <button class='delete-btn'>X</button>
+        <li class= 'link-item'>
+         <a href= '${url}' target='_blank'>${nombre}</a>
+         <button class='delete-btn' data-index='${index}'>X</button>
         </li>`;
        ulList.innerHTML += template;
     })
 };
-*/
 
+storagebtn.addEventListener('click', () => {
+    renderizar(storeData());
+});
 
+ulList.addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete-btn')) {
+        const index = e.target.dataset.index;
+        const storeItem = localStorage.getItem('data');
+        const saveLinks = storeItem ? JSON.parse(storeItem) : [];
+
+        saveLinks.splice(index, 1);
+        localStorage.setItem('data', JSON.stringify(saveLinks)); 
+        renderizar(saveLinks); 
+    }
+});
